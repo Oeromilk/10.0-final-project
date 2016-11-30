3d0184ac52f5e749bf52be465e0b409b9c3275cc
Oeromilk <oero.mccomb@gmail.com>
2016-11-29 23:21:09 -0500

    adjust navbar not finished yet

--- app/scripts/components/templates/template.jsx
+++ app/scripts/components/templates/template.jsx
@@ -26,27 +26,29 @@ var FixedHeader = React.createClass({
           <div className="navbar-header hover-style">
             <a className="navbar-brand" href="#">Shelf</a>
           </div>
-          <ul className="nav navbar-nav">
+          <ul className="nav navbar-nav visible-sm visible-md visible-lg">
             <li><a href="#user-listing/"><i className="fa fa-list" aria-hidden="true"></i></a></li>
             <li><a href="#top-users/"><i className="fa fa-fire" aria-hidden="true"></i></a></li>
             <li><a href="#date-picker/"><i className="fa fa-calendar-o" aria-hidden="true"></i></a></li>
           </ul>
           <ul className="nav navbar-nav navbar-right">
-            <li><p className="navbar-text">Signed in as: {username}</p></li>
-            <li><div className="navbar-brand"><img className="user-nav-avatar img-rounded" src={userAvatar} /></div></li>
-            <li><a href="#login/" role="button">Sign In <i className="fa fa-sign-in" aria-hidden="true"></i></a></li>
-              <li className="dropdown">
+            <li><p className="navbar-text visible-sm visible-md visible-lg">Signed in as: {username}</p></li>
+            <li><div className="navbar-brand visible-sm visible-md visible-lg"><img className="user-nav-avatar img-rounded" src={userAvatar} /></div></li>
+            <li><a className="visible-sm visible-md visible-lg" href="#login/" role="button">Sign In <i className="fa fa-sign-in" aria-hidden="true"></i></a></li>
+              <li id="navBarDropDown" className="dropdown visible-xs">
                 <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i className="fa fa-bars" aria-hidden="true"></i></a>
                 <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
+                  <li><a href="#user-listing/"><i className="fa fa-list" aria-hidden="true"></i></a></li>
+                  <li><a href="#top-users/"><i className="fa fa-fire" aria-hidden="true"></i></a></li>
+                  <li><a href="#date-picker/"><i className="fa fa-calendar-o" aria-hidden="true"></i></a></li>
                   <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#login/" role="button">Sign In <i className="fa fa-sign-in" aria-hidden="true"></i></a></li>
+                  <li><a href="#signup/" role="button">Sign Up <i className="fa fa-user-plus" aria-hidden="true"></i></a></li>
+                  <li><a onClick={this.handleSignOut} href="#"><i className="fa fa-power-off" aria-hidden="true"></i></a></li>
                 </ul>
               </li>
-            <li><a href="#signup/" role="button">Sign Up <i className="fa fa-user-plus" aria-hidden="true"></i></a></li>
-            <li><a onClick={this.handleSignOut} href="#"><i className="fa fa-power-off" aria-hidden="true"></i></a></li>
+            <li><a className="visible-sm visible-md visible-lg" href="#signup/" role="button">Sign Up <i className="fa fa-user-plus" aria-hidden="true"></i></a></li>
+            <li><a className="visible-sm visible-md visible-lg" onClick={this.handleSignOut} href="#"><i className="fa fa-power-off" aria-hidden="true"></i></a></li>
           </ul>
         </div>
       </nav>
